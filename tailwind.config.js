98aeb283a9256119817c69d1f1cb8c98a078e99f
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-10-03 23:05:13 +0200

    remade construction page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: ["./src/**/*.{html,js,astro}"],
+    content: ["./src/**/*.{html,js,astro,css}"],
     theme: {
-      extend: {},
+
+      colors: {
+        'primary': '#05192d',
+        'secondary': '#68ebb8',
+        'background': '#f9f4f0',
+        'grey-100': '#d1d5db',
+      },
+
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px',
+      },
+
+      extend: {
+        fontFamily: {
+          button: ['Teko', 'sans-serif'],
+          body: ['Averta', 'sans-serif']
+        }
+      },
+      
     },
     plugins: [],
   }
