e2d45020dc83972a17c3cb2deb50357c2607c969
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-10-09 23:07:57 +0200

    feature: mobile nav

--- src/components/Mobilenav.jsx
+++ src/components/Mobilenav.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Popover } from '@headlessui/react'
-import Hamburger from '../assets/lottie/hamburger/hamburger';
+import Hamburger from '../assets/lottie/hamburger/Hamburger';
 
 
 function Mobilenav() {
     return (
       <Popover className="relative">
-      <Popover.Button><Hamburger /></Popover.Button>
-      <Popover.Overlay/>
-      <Popover.Panel className="absolute z-10 inset-x-0 top-20 bottom-0">
-        <div className="flex flex-col fixed inset-0 justify-center items-center backdrop-blur">
-          <a className=' font-button text-primary' href="/analytics">Analytics</a>
-          <a className=' font-button text-primary' href="/engagement">Engagement</a>
-          <a className=' font-button text-primary' href="/security">Security</a>
-          <a className=' font-button text-primary' href="/integrations">Integrations</a>
-        </div>
-      </Popover.Panel>
+        
+        <Popover.Button>
+          <Hamburger />
+        </Popover.Button>
+
+        <Popover.Panel className="absolute z-10 backdrop-blur">
+          <div className="flex flex-col fixed inset-x-0 bottom-0 justify-center items-center">
+            <a className="font-button text-primary" href="#">Home</a>
+            <a className="font-button text-primary" href="#">About</a>
+            <a className="font-button text-primary" href="#">Contact</a>
+            <a className="font-button text-primary" href="#">Start project</a>
+          </div>
+        </Popover.Panel>
     </Popover>
   )
 }
