ad348361d14a740df16627e7995da92eddf42b3c
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-10-11 21:54:01 +0200

    fix: navigation zindex

--- src/components/Mobilenav.jsx
+++ src/components/Mobilenav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Popover } from '@headlessui/react'
+import { Popover, Transition } from '@headlessui/react'
 import Hamburger from '../assets/lottie/hamburger/Hamburger';
 
 
@@ -11,14 +11,25 @@ function Mobilenav() {
           <Hamburger />
         </Popover.Button>
 
-        <Popover.Panel className="relative flex flex-col z-50">
-          <div className="fixed flex flex-col z-10 inset-0 h-full justify-center items-center bg-secondary">
+        <Transition
+        enter="transition duration-500"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition duration-500"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+                <Popover.Panel className="relative flex flex-col z-50">
+          <div className="fixed flex flex-col inset-x-0 top-24 h-full justify-center items-center overflow-hidden">
             <a className="font-button text-primary uppercase text-base tracking-wider font-medium my-2" href="#">Home</a>
             <a className="font-button text-primary uppercase text-base tracking-wider font-medium my-2" href="#">About</a>
             <a className="font-button text-primary uppercase text-base tracking-wider font-medium my-2" href="#">Contact</a>
             <a className="font-button text-primary uppercase text-base tracking-wider font-medium my-2" href="#">Start project</a>
           </div>
         </Popover.Panel>
+      </Transition>
+
+
     </Popover>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 
 module.exports = {
-    content: ["./src/**/*.{html,js,astro,css}"],
+  content: ["./src/**/*.{html,js,jsx,astro,css}"],
     theme: {
 
       extend: {
