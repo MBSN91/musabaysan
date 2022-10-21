2a4f1e75fc6e389c2a3ab7266d458afcc4be8f44
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-10-21 23:49:58 +0200

    fix: img optim

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@ import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
 import react from '@astrojs/react';
 
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+	integrations: [tailwind(), react(), image()],
 	output: 'server',
 	adapter: netlify(),
 });
