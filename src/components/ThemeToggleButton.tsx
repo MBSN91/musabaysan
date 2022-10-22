8f77718e78dbfe47bc84af58c28553c86087768e
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-10-22 19:49:14 +0200

    fix: img optim v3

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { IoSunny, IoMoon } from 'react-icons/io5/index.js';
 
 const themes = ['light', 'dark'];
