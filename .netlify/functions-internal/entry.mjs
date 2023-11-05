c956aa9be54c24d8e6479435a68bdcc627f1146a
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2023-11-05 23:09:09 +0100

    update v3 astro

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_6840f3e7.mjs';
+import { manifest } from './manifest_3c227d64.mjs';
 import 'react';
 import 'react-dom/server';
 import 'cookie';
